766ecf0a0828399632e87bc3c725a2622245a3c2
Clayton <sbncla002@myuct.ac.za>
2017-08-26 12:37:10 +0200

    now have included the code for the experiment

--- src/MainParrallel.java
+++ src/MainParrallel.java
@@ -1,28 +1,108 @@
-import java.util.*;
-import java.util.concurrent.ForkJoinPool;
-
 
 /**
  * Created by Clayza on 2017-08-25.
  */
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.ForkJoinPool;
+
+
 public class MainParrallel {
 
+    public static void main(String args[]) {
 
-    public static  void main(String args [])
+        for(int i=0;i<inputSize().length;i++) {
+            double arr[] = readFile("C:\\Users\\USER\\Desktop\\2nd year\\2nd Semester\\CSC2002S\\Assignments\\sampleinputfile.txt",inputSize()[i]);
+            double newArr[] = new double[arr.length];
+            //Scanner sc = new Scanner(System.in);
+            // System.out.println("Enter the size of the window: ");
+            int size = 3;// sc.nextInt();
+
+           // while (size != 0) {
+                double tempArr[] = new double[size];
+            long start_time = System.nanoTime();
+                ForkJoinPool.commonPool().invoke(new MedianParrallel((size - 1) / 2, arr.length - (size - 1) / 2, arr, newArr, tempArr, size));
+            long end_time = System.nanoTime();
+                writeFile("C:\\Users\\USER\\Desktop\\2nd year\\2nd Semester\\CSC2002S\\Assignments\\Assignment1\\output.txt", newArr);
+                 System.out.println("length size time");
+                System.out.println(arr.length+" "+size+" "+((end_time - start_time) / 1e6));
+                // System.out.println("Enter the size of the window: ");
+                //size = sc.nextInt();
+            //}
+        }
+
+    }
+
+
+    public static double[] readFile(String filename,int arrLength) //reads the file and returns an array of doubles
     {
-double arr[] = {2,80,6,3,1};
-double newArr[] = new double[arr.length];
-Scanner sc = new Scanner(System.in);
+        double[] arr = null;
+        try {
+            FileReader fr = new FileReader(filename);
 
-int size=sc.nextInt();
-double tempArr[] =new double[size];
-ForkJoinPool.commonPool().invoke(new MedianParrallel((size-1)/2,arr.length-(size-1)/2,arr,newArr,tempArr,size));
-System.out.println("The Array is "+Arrays.toString(newArr));
+            BufferedReader bf = new BufferedReader(fr);
+            String line = "";
+            int length = Integer.parseInt(bf.readLine());
+           // System.out.println(length);
+            arr = new double[arrLength];
+            int i = 0;
 
+                while ((line = bf.readLine()) != null && i<arrLength) {
+
+                    arr[i] = Double.parseDouble((line.split(" ")[1]));
+                    i++;
+                }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return arr;
+
+    }
+
+    public static void writeFile(String filename, double arr[]) {
+        try {
+            FileWriter fw = new FileWriter(filename);
+            String newLine = System.getProperty("line.separator");
+            fw.write(arr.length + newLine);
+            for (int i = 0; i < arr.length; i++) {
+                fw.write(i + 1 + " " + arr[i] + newLine);
+            }
+            fw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
 
 
+//This method is for taking in the input sizes for the experiment
+private static int[] inputSize()
+{
+    int arr[] = new int[10];
+    arr[0]=50000;
+    arr[1]=100000;
+    for(int i=2;i<arr.length;i++)
+    {
+      arr[i]=arr[i-1]+100000;
+    }
+   return arr;
 }
 
+private int[] filterSize()
+{
+    int arr[] = new int[10];
+    for(int i=0,j=3;i<arr.length;i++,j+=2)
+    {
+        arr[i]=j;
+    }
+    return arr;
+}
+
+
+}
--- src/MedianParrallel.java
+++ src/MedianParrallel.java
@@ -3,87 +3,75 @@ import java.util.concurrent.RecursiveAction;
 /**
  * Created by Clayza on 2017-08-25.
  */
-public class MedianParrallel extends RecursiveAction
-{
- int low;
- int high;
- double arr [];
- double newArr [];
- double tempArr[];
- int size;
- int SEQUENTIAL_CUTOFF=1000;
+public class MedianParrallel extends RecursiveAction {
+    int low;
+    int high;
+    double arr[];
+    double newArr[];
+    double tempArr[];
+    int size;
+    int SEQUENTIAL_CUTOFF = 1000;
 
- public MedianParrallel(int low, int high,double arr[],double newArr[],double tempArr[],int size)
-    {
-        this.low=low;
-        this.high=high;
-        this.arr=arr;
-        this.newArr=newArr;
-        this.size=size;
-        this.tempArr=tempArr;
+    public MedianParrallel(int low, int high, double arr[], double newArr[], double tempArr[], int size) {
+        this.low = low;
+        this.high = high;
+        this.arr = arr;
+        this.newArr = newArr;
+        this.size = size;
+        this.tempArr = tempArr;
 
     }
 
-   @Override
-public void compute()
-{
+    @Override
+    public void compute() {
 
-    for(int i=0;i<(size-1)/2;i++)
-    {
-        newArr[i]=arr[i];
-    }
-
-    for(int i=arr.length-(size-1)/2;i<arr.length;i++)
-    {
-        newArr[i]=arr[i];
-    }
-
-    if(high-low<SEQUENTIAL_CUTOFF)
-    {
-        int k=0;
-        //System.out.println(Arrays.toString(newArr));
-
-        for(int i=low;i<high;i++)
-        {
-            k=0;
-            for(int j=i-(size/2);j<=i+(size/2);j++)//for populating temp Array
-            {System.out.println("j is "+j+" and i is "+i);
-                tempArr[k]=arr[j];
-                k++;
-            }
-            //System.out.println("Temp arry is "+Arrays.toString(tempArr));
-            newArr[i]=median(tempArr);
-
+        for (int i = 0; i < (size - 1) / 2; i++) {
+            newArr[i] = arr[i];
         }
+
+        for (int i = arr.length - (size - 1) / 2; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+
+        if (high - low < SEQUENTIAL_CUTOFF) {
+            int k = 0;
+            //System.out.println(Arrays.toString(newArr));
+
+            for (int i = low; i < high; i++) {
+                k = 0;
+                for (int j = i - (size / 2); j <= i + (size / 2); j++)//for populating temp Array
+                {//System.out.println("j is "+j+" and i is "+i);
+                    tempArr[k] = arr[j];
+                    k++;
+                }
+                //System.out.println("Temp arry is "+Arrays.toString(tempArr));
+                newArr[i] = median(tempArr);
+
+            }
+        } else {
+            MedianParrallel left = new MedianParrallel(low, (low + high) / 2, arr, newArr, tempArr, size);
+            MedianParrallel right = new MedianParrallel((low + high) / 2, high, arr, newArr, tempArr, size);
+            left.fork();
+            right.compute();
+            left.join();
+        }
+
+
     }
 
-    else
-    {
-        MedianParrallel left = new MedianParrallel(low,(low+high)/2,arr,newArr,tempArr,size);
-        MedianParrallel right = new MedianParrallel((low+high)/2,high,arr,newArr,tempArr,size);
-       left.fork();
-       right.compute();
-       left.join();
-    }
 
-
-}
-
-
-    public double median(double arr[])
-    {
+    public double median(double arr[]) {
 
         MergeSort ms = new MergeSort();
 
-        arr=ms.mergeSort(arr);
+        arr = ms.mergeSort(arr);
 
 
-
-        if((arr.length)%2!=0)
-            return arr[arr.length/2];
+        if ((arr.length) % 2 != 0)
+            return arr[arr.length / 2];
 
         else
-            return (arr[arr.length/2]+arr[(arr.length/2)-1])/2;
+            return (arr[arr.length / 2] + arr[(arr.length / 2) - 1]) / 2;
 
     }
 
